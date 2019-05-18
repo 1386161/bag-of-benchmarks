da60cb1c79a5900fc246d1489edf96a7bb7190aa
Nathan Michlo <xuraiis@gmail.com>
2019-05-18 23:37:07 +0200

    flushing

--- src/util_mpi4py.py
+++ src/util_mpi4py.py
@@ -2,11 +2,12 @@
   Some utility functions useful for MPI parallel programming
 """
 from mpi4py import MPI
+from termcolor import cprint
 
 #=============================================================================
 # I/O Utilities
 
 def mprint(string="", end="\n", comm=MPI.COMM_WORLD):
     """Print for MPI parallel programs: Only rank 0 prints *str*."""
-    if comm.rank == 0:
-        print(string + end)
+    cprint(str(comm.rank) + ': ', end='')
+    print(string + end, flush=True)
